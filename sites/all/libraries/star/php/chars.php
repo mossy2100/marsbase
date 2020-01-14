1a13586356ef5c39214cd1ccca0e72299434c7b8
Shaun Moss <shaun@astromultimedia.com>
2020-01-14 12:07:37 +1000

    Changed curly braces as used for character access in strings to square brackets.

--- sites/all/libraries/star/php/chars.php
+++ sites/all/libraries/star/php/chars.php
@@ -21,7 +21,7 @@ function convertHtmlEntities($str) {
 function htmlEntitiesAll($str) {
   $result = "";
   for ($i = 0; $i < strlen($str); $i++)
-    $result .= "&#".ord($str{$i}).";";
+    $result .= "&#".ord($str[$i]).";";
   return $result;
 }
 
--- sites/all/libraries/star/php/strings.php
+++ sites/all/libraries/star/php/strings.php
@@ -308,7 +308,7 @@ function stripWhitespace($str) {
  */
 function containsDigits($str) {
   for ($i = 0; $i < strlen($str); $i++)  {
-    if (ctype_digit($str{$i})) {
+    if (ctype_digit($str[$i])) {
       return TRUE;
     }
   }
