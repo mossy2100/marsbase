ba255282adcd72115bf7863ed8e9c4e7fd3f68ef
Shaun Moss <shaun@astromultimedia.com>
2020-03-29 22:09:19 +1000

    Removed old code from gulpfile.

--- sites/all/themes/custom/imrs/gulpfile.js
+++ sites/all/themes/custom/imrs/gulpfile.js
@@ -1,29 +1,9 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const compass = require('gulp-compass');
 const clean = require('gulp-clean');
-
-// gulp.task('compass', function() {
-//   gulp.src('./sass/*.scss')
-//     .pipe(compass({
-//       project: __dirname,
-//       css: 'css',
-//       sass: 'sass'
-//     }))
-//     .pipe(gulp.dest('./css'));
-// });
-
-// const sassProd = () => {
-//   gulp.src('./sass/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(autoprefixer({
-//        browsers: ['last 2 version']
-//     }))
-//     .pipe(gulp.dest('./css'));
-// };
 
 /**
  * Delete the CSS files.
@@ -46,14 +26,13 @@ const compileScss = () => {
       debug: true,
       environment: 'development'
     }))
-    // .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
 };
 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.scss', ['sass:dev']);
-// });
+const watch = () => {
+   gulp.watch('./sass/**/*.scss', ['compileScss']);
+};
 
-// gulp.task('default', gulp.series('sass:dev', 'sass:watch'));
 exports.default = gulp.series(deleteCssFiles, compileScss);
+exports.watch = watch;
